f42713c16cd75d5b192dfd83921c1a6a0e6b0a37
Toralf Förster <toralf.foerster@gmx.de>
2022-09-08 22:44:34 +0200

    run DNS updater not in parallel

--- bin/update-dns.sh
+++ bin/update-dns.sh
@@ -11,30 +11,45 @@ project=${1:?}
 
 hconf=/etc/unbound/hetzner-${project}.conf
 if ! grep -q "include:.*${hconf}" /etc/unbound/unbound.conf; then
-  echo -e "\n unbound has to be configured to include ${hconf} !\n"
+  echo -e "\n unbound does not use ${hconf} ?!\n"
   exit 1
 fi
 
 hcloud context use ${project}
 
-echo 'server:' | sudo tee ${hconf} 1>/dev/null
-sudo chmod 644 ${hconf}
+# do not run in parallel
+if [[ -f ${hconf}.new ]]; then
+  echo -n " $(date) found ${hconf}.new, waiting "
+  while [[ -e ${hconf}.new ]]
+  do
+    echo -n '.'
+    sleep 2
+  done
+  date
+fi
+sudo touch ${hconf}.new
 
-hcloud server list |\
-# ID   NAME   STATUS   IPV4   IPV6   DATACENTER
-awk '! /ID/ { print $2, $4, $5 }' |\
-sort |\
-while read -r name ip4 ip6mask
-do
-  ip6=$(sed -e 's,/64,1,' <<< ${ip6mask})   # Debian defaults to [...:1]
-  printf "  local-data:     \"%-40s  A     %s\"\n" ${name} ${ip4}
-  printf "  local-data:     \"%-40s  AAAA  %s\"\n" ${name} ${ip6}
-  printf "  local-data-ptr: \"%-40s        %s\"\n" ${ip4} ${name}
-  printf "  local-data-ptr: \"%-40s        %s\"\n" ${ip6} ${name}
-done |\
-sudo tee -a ${hconf} 1>/dev/null
+(
+  echo 'server:'
 
-echo
-sudo /sbin/rc-service unbound reload
-echo
+  hcloud server list |\
+  # ID   NAME   STATUS   IPV4   IPV6   DATACENTER
+  awk '! /ID/ { print $2, $4, $5 }' |\
+  sort |\
+  while read -r name ip4 ip6mask
+  do
+    ip6=$(sed -e 's,/64,1,' <<< ${ip6mask})   # Debian defaults to [...:1]
+    printf "  local-data:     \"%-40s  A     %s\"\n" ${name} ${ip4}
+    printf "  local-data:     \"%-40s  AAAA  %s\"\n" ${name} ${ip6}
+    printf "  local-data-ptr: \"%-40s        %s\"\n" ${ip4} ${name}
+    printf "  local-data-ptr: \"%-40s        %s\"\n" ${ip6} ${name}
+  done
+) | sudo tee ${hconf}.new 1>/dev/null
 
+# no unneeded reload
+if ! diff -q ${hconf}.new ${hconf}; then
+  sudo cp ${hconf}.new ${hconf}
+  sudo /sbin/rc-service unbound reload
+fi
+
+sudo rm ${hconf}.new
