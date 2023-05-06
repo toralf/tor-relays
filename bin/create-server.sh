108afb5c8fe69b78e7463cdd8c66cb6c525bdff1
Toralf Förster <toralf.foerster@gmx.de>
2023-05-06 18:43:17 +0200

    prefer cax11

--- bin/create-server.sh
+++ bin/create-server.sh
@@ -10,17 +10,16 @@ export PATH=/usr/sbin:/usr/bin:/sbin/:/bin
 project=$(hcloud context active)
 [[ -n $project ]]
 
-locations=$(hcloud location list --output columns=name | awk '{ if (NR > 1) { print $1 } }')
+cax11_id=$(hcloud server-type list --output json | jq -cr '.[] | select(.name=="cax11") | .id')  # prefer arm64
+datacenter_list=$(hcloud datacenter list --output json)
+locations=$(hcloud location list --output json | jq -cr '.[].name')
 
-while read -r i; do
-  if ! hcloud server describe $i 2>/dev/null | grep -e "^Name:" -e "^Status:" -e "^Created:" -e "^    IP:"; then
-    loc=$(shuf -n 1 <<<${locations})
-    case $loc in
-    fsn1) size="cax11" ;;
-    *) size="cpx11" ;;
-    esac
-    echo "$loc $size"
-    hcloud server create --name $i --location $loc --image "debian-11" --ssh-key "id_ed25519.pub" --type $size --poll-interval 2s
+while read -r name; do
+  if ! hcloud server describe $name 2>/dev/null | grep -e "^Name:" -e "^Status:" -e "^Created:" -e "^    IP:"; then
+    loc=$(shuf -n 1 <<<$locations)
+    model=$(jq -cr '.[] | select(.location.name == "'$loc'") | .server_types.available | if contains(['$cax11_id']) then "cax11" else "cpx11" end' <<<$datacenter_list)
+    echo "$loc $model"
+    hcloud server create --name $name --location $loc --image "debian-11" --ssh-key "id_ed25519.pub" --type $model --poll-interval 2s
     echo
   fi
 done < <(xargs -n 1 <<<$*)
@@ -29,7 +28,7 @@ echo
 $(dirname $0)/update-dns.sh
 
 echo -n 'add to known_hosts '
-for i in $(seq 1 15); do
+for i in $(seq 1 12); do
   echo -n '.'
   sleep 1
 done
