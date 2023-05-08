3a12b934a99d85e15162ec41d55a3e6560658fae
Toralf Förster <toralf.foerster@gmx.de>
2023-05-08 21:23:47 +0200

    fix stdin handling

--- bin/add-to-known_hosts.sh
+++ bin/add-to-known_hosts.sh
@@ -9,6 +9,5 @@ export PATH=/usr/sbin:/usr/bin:/sbin/:/bin
 [[ $# -ne 0 ]]
 
 echo -n " add to ~/.ssh/known_hosts ... "
-# shellcheck disable=SC2261
-xargs -r -n 1 -P $(nproc) ssh -q -oStrictHostKeyChecking=accept-new -oConnectTimeout=1 -oConnectionAttempts=6 </dev/null 1>/dev/null <<<$*
+xargs -r -n 1 -P $(nproc) ssh -q -oStrictHostKeyChecking=accept-new -oConnectTimeout=1 -oConnectionAttempts=6 -n 1>/dev/null <<<$*
 echo
