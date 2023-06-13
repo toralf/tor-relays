0da61452e050445d48ff6be21a223d14aecd37bb
Toralf Förster <toralf.foerster@gmx.de>
2023-06-13 18:03:34 +0200

    handle already halted server

--- bin/delete-server.sh
+++ bin/delete-server.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # set -x
 
-set -eu
+set -euf
 export LANG=C.utf8
 export PATH=/usr/sbin:/usr/bin:/sbin/:/bin
 
@@ -10,25 +10,29 @@ hash -r hcloud
 
 [[ $# -ne 0 ]]
 project=$(hcloud context active)
-[[ -n $project ]]
+[[ -n ${project} ]]
 
 jobs=$((1 * $(nproc)))
 
 echo -n " stopping tor service(s) ..."
-xargs -n 1 <<<$* | xargs -r -P ${jobs} -I {} ssh -n {} "service tor stop &>/dev/null || true" 1>/dev/null
-echo
+if xargs -n 1 <<<$* | xargs -r -P ${jobs} -I {} ssh -n -oConnectTimeout=2 -oConnectionAttempts=1 {} "service tor stop &>/dev/null 1>/dev/null || true"; then
+  echo
+  sleep 5
+fi
 
 echo -n " delete from ~/.ssh/known_hosts and tmp files ... "
 while read -r name; do
-  sed -i -e "/^$name /d" ~/.ssh/known_hosts ~/tmp/public_*
+  set +e
+  sed -i -e "/^${name} /d" ~/.ssh/known_hosts ~/tmp/public_{bto,clients,onionoo,uname,uptime,version}
+  sed -i -e "/ # ${name}$/d" /tmp/public_bridgeline
+  set -e
 done < <(xargs -n 1 <<<$*)
 echo
 
-sleep 5
-
 echo -n " deleting server(s) ..."
-xargs -r -n 1 -P ${jobs} hcloud server delete 1>/dev/null <<<$*
-echo
+if xargs -r -n 1 -P ${jobs} hcloud server delete 1>/dev/null <<<$*; then
+  echo
+fi
 
 echo
 $(dirname $0)/update-dns.sh
