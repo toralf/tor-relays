414964359a2e87e32a456e1e5fbe17be8d284e1c
Toralf Förster <toralf.foerster@gmx.de>
2023-06-13 18:02:37 +0200

    choose latest Debian

--- bin/create-server.sh
+++ bin/create-server.sh
@@ -10,20 +10,21 @@ hash -r hcloud jq
 
 [[ $# -ne 0 ]]
 project=$(hcloud context active)
-[[ -n $project ]]
+[[ -n ${project} ]]
 
 jobs=$((1 * $(nproc)))
 
 cax11_id=$(hcloud server-type list --output json | jq -cr '.[] | select(.name=="cax11") | .id') # prefer arm64
-cax11_locations=$(hcloud datacenter list --output json | jq -cr '.[] | select(.server_types.available | contains(['$cax11_id'])) | .location.name')
+cax11_locations=$(hcloud datacenter list --output json | jq -cr '.[] | select(.server_types.available | contains(['${cax11_id}'])) | .location.name')
 all_locations=$(hcloud location list --output json | jq -cr '.[].name')
+os_version=$(hcloud image list -t system -output columns=name | grep '^debian' | sort -n | tail -n 1)
 
 while read -r name; do
-  loc=$(shuf -n 1 <<<$all_locations)
-  [[ " $cax11_locations " =~ " $loc " ]] && model="cax11" || model="cpx11"
-  echo "--name $name --location $loc --type $model"
+  loc=$(shuf -n 1 <<<${all_locations})
+  [[ " ${cax11_locations} " =~ " ${loc} " ]] && model="cax11" || model="cpx11"
+  echo "--name ${name} --location ${loc} --type ${model}"
 done < <(xargs -n 1 <<<$*) |
-  xargs -r -P ${jobs} -I {} bash -c "hcloud server create --image debian-11 --ssh-key id_ed25519.pub --poll-interval 2s {}"
+  xargs -r -P ${jobs} -I {} bash -c "hcloud server create --image ${os_version} --ssh-key id_ed25519.pub --poll-interval 2s {}"
 
 echo
 $(dirname $0)/update-dns.sh
