3c8f4d5e4fb22d2718b04283765e33dd737fd023
Toralf Förster <toralf.foerster@gmx.de>
2023-06-26 21:05:53 +0200

    refactor into common, Tor and Snowflake part

--- bin/delete-server.sh
+++ bin/delete-server.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # set -x
 
-set -euf
+set -uf
 export LANG=C.utf8
 export PATH=/usr/sbin:/usr/bin:/sbin/:/bin
 
@@ -14,17 +14,11 @@ project=$(hcloud context active)
 
 jobs=$((1 * $(nproc)))
 
-echo -n " stopping tor service(s) ..."
-if xargs -n 1 <<<$* | xargs -r -P ${jobs} -I {} ssh -n -oConnectTimeout=1 -oConnectionAttempts=1 {} "service tor stop &>/dev/null >/dev/null || true"; then
-  echo
-fi
+echo -e "\n delete ssh hash(es) ... "
+xargs -r -n 1 -P ${jobs} ssh-keygen -R <<<$*
+echo $?
 
-echo -n " delete ssh hash(es) ... "
-if xargs -r -n 1 -P ${jobs} ssh-keygen -R <<<$*; then
-  echo
-fi
-
-echo -n " delete .ansible_facts and line(s) in tmp files ... "
+echo -e "\n delete .ansible_facts and line(s) in tmp files ... "
 while read -r name; do
   set +e
   sed -i -e "/^${name} /d" ~/tmp/${project}_* 2>/dev/null
@@ -32,9 +26,8 @@ while read -r name; do
   set -e
   rm -f $(dirname $0)/../.ansible_facts/${name}
 done < <(xargs -n 1 <<<$*)
-echo
 
-echo -n " collect protected ip(s) ..."
+echo -e "\n collect protected ip(s) ..."
 ids=""
 while read -r name; do
   for v in ipv4 ipv6; do
@@ -44,18 +37,15 @@ while read -r name; do
     fi
   done
 done < <(xargs -n 1 <<<$*)
-echo
 if [[ -n ${ids} ]]; then
-  echo -n " unprotect $(wc -l <<<${ids}) ip(s) ..."
-  if xargs -r -n 1 -P ${jobs} hcloud primary-ip update --auto-delete=true <<<${ids}; then
-    echo
-  fi
+  echo -e "\n unprotect $(wc -l <<<${ids}) ip(s) ..."
+  xargs -r -n 1 -P ${jobs} hcloud primary-ip update --auto-delete=true <<<${ids}
+  echo $?
 fi
 
-echo -n " delete server(s) ..."
-if xargs -r -n 1 -P ${jobs} hcloud server delete <<<$*; then
-  echo
-fi
+echo -e "\n delete server(s) ..."
+xargs -r -n 1 -P ${jobs} hcloud server delete <<<$*
+echo $?
 
 echo
 $(dirname $0)/update-dns.sh
