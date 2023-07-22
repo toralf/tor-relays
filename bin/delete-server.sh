57cf54fd77d2a9c1fb2c243c0dba019a12b73fb1
Toralf Förster <toralf.foerster@gmx.de>
2023-07-22 18:47:58 +0200

    fix set -f misuse

--- bin/delete-server.sh
+++ bin/delete-server.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # set -x
 
-set -uf # no -e here
+set -u # no -ef here
 export LANG=C.utf8
 export PATH=/usr/sbin:/usr/bin:/sbin/:/bin
 
@@ -17,7 +17,7 @@ jobs=$((1 * $(nproc)))
 echo -e "\n delete known ssh key(s), .ansible_facts and line(s) in tmp files ... "
 while read -r name; do
   sed -i -e "/^${name} /d" -e "/^${name},/d" ~/.ssh/known_hosts 2>/dev/null
-  sed -i -e "/^${name} /d" ~/tmp/${project}_* 2>/dev/null
+  sed -i -e "/^${name} /d" -e "/^${name}$/d" ~/tmp/${project}_* 2>/dev/null
   sed -i -e "/ # ${name}$/d" /tmp/${project}_bridgeline 2>/dev/null
   rm -f $(dirname $0)/../.ansible_facts/${name}
 done < <(xargs -n 1 <<<$*)
