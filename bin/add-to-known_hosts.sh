bf4b05b022de304fcba626d35c1b542c162b3b3d
Toralf Förster <toralf.foerster@gmx.de>
2023-08-01 19:25:41 +0200

    remove an echo

--- bin/add-to-known_hosts.sh
+++ bin/add-to-known_hosts.sh
@@ -11,9 +11,7 @@ export PATH=/usr/sbin:/usr/bin:/sbin/:/bin
 jobs=$((1 * $(nproc)))
 
 echo " add to ~/.ssh/known_hosts: "
-if xargs -n 1 <<<$* | xargs -r -P ${jobs} -I '{}' ssh -n -oStrictHostKeyChecking=accept-new -oConnectTimeout=2 -oConnectionAttempts=2 {} "uname -a"; then
-  echo
-else
+if ! xargs -n 1 <<<$* | xargs -r -P ${jobs} -I '{}' ssh -n -oStrictHostKeyChecking=accept-new -oConnectTimeout=2 -oConnectionAttempts=2 {} "uname -a"; then
   echo -e "\n some failed ^^\n"
   exit 1
 fi
