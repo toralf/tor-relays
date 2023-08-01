15e29ade42fbf8759cfabd71764ce3343788429a
Toralf Förster <toralf.foerster@gmx.de>
2023-08-01 09:56:52 +0200

    respect env vars + fix timing issues

--- bin/add-to-known_hosts.sh
+++ bin/add-to-known_hosts.sh
@@ -11,6 +11,9 @@ export PATH=/usr/sbin:/usr/bin:/sbin/:/bin
 jobs=$((1 * $(nproc)))
 
 echo " add to ~/.ssh/known_hosts: "
-xargs -n 1 <<<$* |
-  xargs -r -P ${jobs} -I '{}' ssh -n -oStrictHostKeyChecking=accept-new -oConnectTimeout=2 -oConnectionAttempts=2 {} "uname -a"
-echo
+if xargs -n 1 <<<$* | xargs -r -P ${jobs} -I '{}' ssh -n -oStrictHostKeyChecking=accept-new -oConnectTimeout=2 -oConnectionAttempts=2 {} "uname -a"; then
+  echo
+else
+  echo -e "\n some failed ^^\n"
+  exit 1
+fi
--- bin/create-server.sh
+++ bin/create-server.sh
@@ -14,18 +14,21 @@ echo -e "\n using Hetzner project ${project:?}"
 
 jobs=$((1 * $(nproc)))
 
-# choose the first one
-ssh_key=$(hcloud ssh-key list --output json | jq -cr '.[].name' | head -n 1)
+ssh_key=${HCLOUD_SSH_KEY:-$(hcloud ssh-key list --output json | jq -cr '.[].name' | head -n 1)}
 
-# 2 vCPU, prefer ARM
-cax11_id=$(hcloud server-type list --output json | jq -cr '.[] | select(.name=="cax11") | .id') # prefer arm64
+# c?x11 == 2 vCPU, prefer ARM (cax11) over AMD (cpx11)
+cax11_id=$(hcloud server-type list --output json | jq -cr '.[] | select(.name=="cax11") | .id')
 cax11_locations=$(hcloud datacenter list --output json | jq -cr '.[] | select(.server_types.available | contains(['${cax11_id}'])) | .location.name')
 all_locations=$(hcloud location list --output json | jq -cr '.[].name')
 os_version=$(hcloud image list -t system --output columns=name | grep '^debian' | sort -u | tail -n 1)
 
 while read -r name; do
-  loc=$(shuf -n 1 <<<${all_locations})
-  [[ " ${cax11_locations} " =~ " ${loc} " ]] && model="cax11" || model="cpx11"
+  loc=${HCLOUD_LOCATION:-$(shuf -n 1 <<<${all_locations})}
+  if [[ " ${cax11_locations} " =~ " ${loc} " ]]; then
+    model="cax11"
+  else
+    model="cpx11"
+  fi
   echo "hcloud server create --image ${os_version} --ssh-key ${ssh_key} --poll-interval 2s --name ${name} --location ${loc} --type ${model}"
 done < <(xargs -n 1 <<<$*) |
   xargs -r -P ${jobs} -I '{}' -t bash -c "{}"
@@ -33,7 +36,10 @@ done < <(xargs -n 1 <<<$*) |
 echo -e "\n add to DNS ..."
 $(dirname $0)/update-dns.sh
 
-echo -e "\n wait 20 sec ..."
-sleep 20
-echo
-$(dirname $0)/add-to-known_hosts.sh $*
+echo -e "\n wait few sec ..."
+sleep 15
+if ! $(dirname $0)/add-to-known_hosts.sh $*; then
+  echo -e "\n wait again ..."
+  sleep 15
+  $(dirname $0)/add-to-known_hosts.sh $*
+fi
