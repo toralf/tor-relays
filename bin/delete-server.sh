7602650277b3cd35ab93b453101274e7e34f5d28
Toralf Förster <toralf.foerster@gmx.de>
2024-02-10 10:21:11 +0100

    fix if current project does not match the inventory group

--- bin/delete-server.sh
+++ bin/delete-server.sh
@@ -21,8 +21,8 @@ jobs=$((2 * $(nproc)))
 echo -e " deleting config(s) ..."
 while read -r name; do
   sed -i -e "/^${name} /d" -e "/^${name},/d" ~/.ssh/known_hosts 2>/dev/null
-  sed -i -e "/^${name} /d" -e "/^${name}$/d" ~/tmp/${project}_* 2>/dev/null
-  sed -i -e "/ # ${name}$/d" /tmp/${project}_bridgeline 2>/dev/null
+  sed -i -e "/^${name} /d" -e "/^${name}$/d" ~/tmp/*_* 2>/dev/null
+  sed -i -e "/ # ${name}$/d" /tmp/*_bridgeline 2>/dev/null
   if grep -q "^    ${name}:$" $(dirname $0)/../inventory/*.yaml 2>/dev/null; then
     # delete host from inventory only if it has no specific vars
     if ! grep -A 1 "^    ${name}:$" $(dirname $0)/../inventory/*.yaml | tail -n 1 | grep -q '^      '; then
