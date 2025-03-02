ef48b6a23af94f359cd233bec46b1075d1042437
Toralf Förster <toralf.foerster@gmx.de>
2025-03-02 12:00:30 +0100

    do not run kernel makes in parallel

--- playbooks/roles/setup_common/files/build-kernel.sh
+++ playbooks/roles/setup_common/files/build-kernel.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
 # set -x
 
 set -euf
@@ -7,18 +8,25 @@ cd ~/linux
 
 suffix=$(date +%Y%m%d-%H%M%S)
 
-make -j ${1:-1} &>/root/make.$suffix.log
-make modules_install &>/root/make-modules_install.$suffix.log
-make install &>/root/make-install.$suffix.log
+truncate -s 0 /root/make.$suffix.log
+rm -f /root/make.log
+ln /root/make.$suffix.log /root/make.log
+
+# shellcheck disable=SC2129
+make -j ${1:-1} &>>/root/make.$suffix.log
+make modules_install &>>/root/make.$suffix.log
+make install &>>/root/make.$suffix.log
 
 kver=$(make kernelversion)
 lver=$(KERNELVERSION=$kver ./scripts/setlocalversion)
 grub_entry="Advanced options for Debian GNU/Linux>Debian GNU/Linux, with Linux $lver"
 sed -i -e "s#^GRUB_DEFAULT=.*#GRUB_DEFAULT=\"$grub_entry\"#" /etc/default/grub
-update-grub &>/root/make-grub.$suffix.log
+update-grub &>>/root/make.$suffix.log
 touch /var/run/reboot-required
 
 ln -snf $PWD /usr/src/linux
+
+rm /root/make.log
 
 if [[ ${2-} == reboot_immediately ]]; then
   reboot
