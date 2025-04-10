6e71af8024850935fefe05539dae9efef09f648b
Toralf Förster <toralf.foerster@gmx.de>
2025-04-10 21:47:28 +0200

    add dep

--- bin/delete-server.sh
+++ bin/delete-server.sh
@@ -6,7 +6,7 @@ set -u # no -ef here
 export LANG=C.utf8
 export PATH=/usr/sbin:/usr/bin:/sbin/:/bin
 
-hash -r hcloud
+hash -r hcloud rc-service
 
 [[ $# -ne 0 ]] || exit 1
 project=$(hcloud context active)
