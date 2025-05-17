88c8912ec0fa8da074338752f5596c5004de69ff
Toralf Förster <toralf.foerster@gmx.de>
2025-05-17 18:37:03 +0200

    spelling/formatting

--- bin/lib.sh
+++ bin/lib.sh
@@ -1,24 +1,27 @@
 # shellcheck shell=bash
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# hint: wellknown entries are not cleaned here
+# hint: ./.wellknown entries are not cleaned
 function cleanLocalData() {
   local d=$(dirname $0)
 
-  echo -e " deleting local system data, DNS and ssl ..."
+  set +e
+
+  echo -e " deleting local data ..."
   while read -r name; do
-    set +e
     [[ -n ${name} ]] || continue
     # Ansible facts
     rm -f $d/../.ansible_facts/${name}
-    # data in ~/tmp
+    # data and files in ~/tmp
     sed -i -e "/^${name} /d" -e "/^${name}$/d" -e "/^${name}:[0-9]*$/d" -e "/\"${name}:[0-9]*\"/d" ~/tmp/*_* 2>/dev/null
     rm -f ~/tmp/*/${name} ~/tmp/*/${name}.*
-    # certs
+    # client certs
     rm -f $d/../secrets/ca/*/clients/{crts,csrs,keys}/${name}.{crt,csr,key}
-    # private Tor bridge data
+    # private Tor bridge line
     sed -i -e "/ # ${name}$/d" /tmp/*_bridgeline 2>/dev/null
   done < <(xargs -n 1 <<<$*)
+
+  set -e
 }
 
 function setImageToLatestSnapshotId() {
