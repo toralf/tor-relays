d2b4e13d87bff04d45df595b8d640b7fceece227
Toralf Förster <toralf.foerster@gmx.de>
2025-07-11 23:45:22 +0200

    do not pin the linux git dir

--- bin/bisect.sh
+++ bin/bisect.sh
@@ -8,20 +8,25 @@ set -euf
 export LANG=C.utf8
 export PATH=/usr/sbin:/usr/bin:/sbin/:/bin
 
-if ! name=${1:?}; then
+if [[ $# -ne 1 ]]; then
   exit 255
 fi
 
+# target system
+name=${1}
+
+# we're called from git bisect run ...
+bisect_id=$(cat .git/BISECT_HEAD) || exit 254
+
 cd $(dirname $0)/..
 
+# maybe dead from previous run
 if ! ping -q -c 3 ${name} 1>/dev/null; then
   ./bin/rebuild-server.sh ${name} || exit 255
 fi
 
-bisect_id=$(cat ~/devel/linux/.git/BISECT_HEAD) || exit 254
-
 # deploy w/o kernel build
 ./site.yaml --limit ${name} --skip-tags kernel-src,auto-update || exit 253
 
-# bisect test
+# bisect: test kernel build and succesful reboot
 ./site.yaml --limit \'${name}\' -e kernel_git_version=${bisect_id} --tags kernel-src --skip-tags auto-update
