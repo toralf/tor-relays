942f06e3913f6fb61fc65b4d6b0c79f335f2ba53
Toralf Förster <toralf.foerster@gmx.de>
2025-07-17 20:41:44 +0200

    hard code to not run in parallel

--- bin/distrust-host-ssh-key.sh
+++ bin/distrust-host-ssh-key.sh
@@ -8,12 +8,9 @@ export PATH=/usr/sbin:/usr/bin:/sbin/:/bin
 
 [[ $# -ne 0 ]]
 
-# file-locking of ssh-keygen doesn't work
-jobs=1
-
 echo -e "\n dis-trusting $(wc -w <<<$*) ssh host key/s ..."
 
-if xargs -r -P ${jobs} -n 1 ssh-keygen -R <<<$* &>/dev/null; then
+if xargs -r -n 1 ssh-keygen -R <<<$* &>/dev/null; then
   echo -e " OK"
 else
   echo -e " NOT ok"
