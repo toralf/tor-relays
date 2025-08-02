014aa749df3f1fd70e3283cfde41ad46b8bdf1d8
Toralf Förster <toralf.foerster@gmx.de>
2025-08-02 11:55:09 +0200

    reduce chink size for server deletion

--- bin/delete-server.sh
+++ bin/delete-server.sh
@@ -34,7 +34,7 @@ $(dirname $0)/distrust-host-ssh-key.sh $*
 echo -e " deleting $(wc -w <<<$*) system/s: $(cut -c -16 <<<$*)..."
 
 set +e
-xargs -r -P ${jobs} -n 50 hcloud --quiet --poll-interval 10s server delete <<<$* 2>/dev/null
+xargs -r -P ${jobs} -n 20 hcloud --quiet --poll-interval 10s server delete <<<$* 2>/dev/null
 rc=$?
 set -e
 [[ ${rc} == 123 ]] && exit 0 || exit ${rc}
