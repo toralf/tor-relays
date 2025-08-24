0f50bb4016c300c8fc30d4df5d16b77ee3b0d056
Toralf Förster <toralf.foerster@gmx.de>
2025-08-24 19:41:49 +0200

    clean up old self-compiled kernels

--- playbooks/roles/setup_common/files/build-kernel.sh
+++ playbooks/roles/setup_common/files/build-kernel.sh
@@ -21,17 +21,23 @@ kver=$(make kernelversion)
 lver=$(KERNELVERSION=$kver ./scripts/setlocalversion)
 grub_entry="Advanced options for Debian GNU/Linux>Debian GNU/Linux, with Linux $lver"
 sed -i -e "s#^GRUB_DEFAULT=.*#GRUB_DEFAULT=\"$grub_entry\"#" /etc/default/grub
+# delete old self-compiled kernels
+grep -E '^\s+initrd\s+/boot/initrd.img-6' /boot/grub/grub.cfg |
+  grep -Eo "img-6\..*-g[0-9a-f]{12}$" |
+  sed -e 's,img\-,,' |
+  while read -r v; do
+    rm /boot/*-${v}
+  done
 update-grub &>>/root/make.$suffix.log
+
 touch /var/run/reboot-required
-
 ln -snf $PWD /usr/src/linux
+rm /root/make.log # indicator that we likely reached this line
 
-rm /root/make.log
-
-# no ssh connection == no ansible task is running
 if [[ ${2-} == "reboot" ]]; then
+  # wait till no ssh connection is open
   while pgrep -af 'sshd:' | grep -v '/usr/sbin/sshd'; do
-    sleep 5
+    sleep 10
   done
   reboot
 fi
