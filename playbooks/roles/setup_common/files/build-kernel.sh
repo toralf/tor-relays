13c894dca1e2475591c5c5712ac479f9c2028c8c
Toralf Förster <toralf.foerster@gmx.de>
2025-08-26 14:06:54 +0200

    clean up old self-compiled kernels

--- playbooks/roles/setup_common/files/build-kernel.sh
+++ playbooks/roles/setup_common/files/build-kernel.sh
@@ -2,37 +2,39 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # set -x
 
-set -euf
+set -eu # no -f
 
 cd ~/linux
 
 suffix=$(date +%Y%m%d-%H%M%S)
 
-truncate -s 0 /root/make.$suffix.log
-rm -f /root/make.log
-ln /root/make.$suffix.log /root/make.log
+truncate -s 0 make.${suffix}.log
+ln -sf make.${suffix}.log make.log
 
 # shellcheck disable=SC2129
-make -j ${1:-1} &>>/root/make.$suffix.log
-make modules_install &>>/root/make.$suffix.log
-make install &>>/root/make.$suffix.log
+# make clean &>>make.log
+make -j ${1:-1} &>>make.log
+make modules_install &>>make.log
+make install &>>make.log
 
 kver=$(make kernelversion)
-lver=$(KERNELVERSION=$kver ./scripts/setlocalversion)
-grub_entry="Advanced options for Debian GNU/Linux>Debian GNU/Linux, with Linux $lver"
-sed -i -e "s#^GRUB_DEFAULT=.*#GRUB_DEFAULT=\"$grub_entry\"#" /etc/default/grub
+lver=$(KERNELVERSION=${kver} ./scripts/setlocalversion)
+grub_entry="Advanced options for Debian GNU/Linux>Debian GNU/Linux, with Linux ${lver}"
+sed -i -e "s#^GRUB_DEFAULT=.*#GRUB_DEFAULT=\"${grub_entry}\"#" /etc/default/grub
 # delete old self-compiled kernels
-grep -E '^\s+initrd\s+/boot/initrd.img-6' /boot/grub/grub.cfg |
-  grep -Eo "img-6\..*-g[0-9a-f]{12}$" |
-  sed -e 's,img\-,,' |
-  while read -r v; do
-    rm /boot/*-${v}
+grep -E '^\s+initrd\s+/boot/initrd.img-6\..*-g[0-9a-f]{12}$' /boot/grub/grub.cfg |
+  awk '{ print $2 }' |
+  sed -e 's,.*img\-,,' |
+  sort -u |
+  grep -v ${lver} |
+  while read -r i; do
+    rm /boot/*-${i}
   done
-update-grub &>>/root/make.$suffix.log
+rm -f /boot/*-6.*-g[0-9a-f]*.old /boot/vmlinuz.old
+update-grub &>>make.log
 
 touch /var/run/reboot-required
-ln -snf $PWD /usr/src/linux
-rm /root/make.log # indicator that we likely reached this line
+ln -snf ${PWD} /usr/src/linux
 
 if [[ ${2-} == "reboot" ]]; then
   # wait till no ssh connection is open
