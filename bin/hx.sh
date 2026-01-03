7b688bc328c8e6c6a90d27f3bee8fd74ac1a456c
Toralf Förster <toralf.foerster@gmx.de>
2026-01-03 19:43:12 +0100

    hx: implement rebuild

--- bin/hx.sh
+++ bin/hx.sh
@@ -103,7 +103,7 @@ trap 'touch /tmp/STOP' INT QUIT TERM EXIT
 while :; do
   echo
 
-  now=${EPOCHSECONDS}
+  start=${EPOCHSECONDS}
 
   # app update
   for i in lyrebird snowflake tor; do
@@ -119,9 +119,6 @@ while :; do
       pit_stop
     fi
   done
-
-  # catch up image build jobs from previous round
-  wait_for_jobs
 
   # kernel update
   for i in ltsrc mainline stablerc; do
@@ -142,32 +139,40 @@ while :; do
     pit_stop
 
     # sshd may died
-    poweroff=$(comm -12 /tmp/is_down.{before,after} | grep "^h" | xargs)
+    poweroff=$(comm -12 /tmp/is_down.{before,after} | grep "^h" | xargs -r)
     if [[ -n ${poweroff} ]]; then
-      info "  take down: $(wc -w <<<${poweroff})"
+      info "  pwoer off/on: $(wc -w <<<${poweroff})"
       xargs -r -n 1 -P 32 echo hcloud --quiet --poll-interval 10s server poweroff <<<${poweroff} &>${log}.poweroff.log || true
+      xargs -r -n 1 -P 32 echo hcloud --quiet --poll-interval 10s server poweron <<<${poweroff} &>${log}.poweron.log || true
       pit_stop
     fi
 
     # trigger missed updates
-    catch_up=$(grep -h "^h" /tmp/is_down.{before,after} | sort -u | xargs)
+    catch_up=$(grep -h "^h" /tmp/is_down.{before,after} | sort -u | xargs -r)
     if [[ -n ${catch_up} ]]; then
       info "  catch up: $(wc -w <<<${catch_up})"
       ./site-setup.yaml --limit "$(tr ' ' ',' <<<${catch_up})" --tags kernel-build,lyrebird,snowflake,tor -e kernel_git_build_wait=false &>${log}.catch_up.log || true
       pit_stop
     fi
 
-    ./site-setup.yaml --limit "$(xargs </tmp/is_down.after | tr ' ' ',')" --tags poweron &>${log}.rebuild.log || true
+    ./site-setup.yaml --limit "$(xargs -r </tmp/is_down.after | tr ' ' ',')" --tags poweron &>${log}.rebuild.log || true
     sort -u ~/tmp/tor-relays/is_down >/tmp/is_down.after_2
-    rebuild=$(comm -12 /tmp/is_down.after{,_2} | grep "^h" | xargs)
-    if [[ -n ${rebuild} ]]; then
-      info "  rebuild: $(wc -w <<<${rebuild})"
+    down=$(comm -12 /tmp/is_down.after{,_2} | grep "^h" | xargs -r)
+    if [[ -n ${down} ]]; then
+      info "  down: $(wc -w <<<${down})"
       wait_for_jobs
+      rebuild=$(xargs -r -n 1 <<<${down} | shuf -n 96 | xargs -r)
+      ./bin/rebuild-server.sh ${rebuild}
+      ./site-setup.yaml --limit "$(tr ' ' ',' <<<${rebuild})" -e kernel_git_build_wait=false &>${log}.rebuild.log || true
       pit_stop
     fi
   fi
 
-  sleep=$((EPOCHSECONDS - now))
+  # catch up image build jobs
+  wait_for_jobs
+
+  pit_stop
+  sleep=$((EPOCHSECONDS - start))
   if [[ ${sleep} -lt 1800 ]]; then
     pit_stop ${sleep}
   fi
