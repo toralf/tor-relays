8edd83f0c4ab113d149b22a710dfee606569e3d8
Toralf Förster <toralf.foerster@gmx.de>
2026-06-12 17:06:30 +0200

    remove debug stmt

--- hx/hx-lib.sh
+++ hx/hx-lib.sh
@@ -11,12 +11,11 @@ function pit_stop() {
   local stopfile=~/tmp/hx/STOP-${1-}
   local sec=${2:-60}
 
-  info "sleep for ${sec}s"
   while ((sec--)) && [[ ! -f ${contfile} && ! -f ${stopfile} ]]; do
     sleep 1
   done
-
   rm -f ${contfile}
+
   if [[ -f ${stopfile} ]]; then
     set +euf
     trap - INT QUIT TERM EXIT
