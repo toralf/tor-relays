9fbbd878626b5922884fbfc92a46099179504d26
Toralf Förster <toralf.foerster@gmx.de>
2026-07-26 13:23:08 +0200

    hx info: no tgas in file names

--- hx/hx-info.sh
+++ hx/hx-info.sh
@@ -3,22 +3,24 @@
 # set -x
 
 function sync_site() {
-  local srv dest log
+  local site=${1?SITE NOT GIVEN}
+  shift
+  local srvs=$(eval echo $*)
 
-  if awk '/^PLAY RECAP/,/^$/' ${logprefix}.${site}.ansible.${tags}.log |
+  if awk '/^PLAY RECAP/,/^$/' ${logprefix}.${site}.ansible.log |
     grep -v -e "^PLAY RECAP" -e " changed=0 " | awk '{ print $0 }' | sort | xargs -r | grep -q .; then
-    for srv in $*; do
-      info "  rsync ${site} to ${srv}"
+    local srv dest log
+    for srv in ${srvs}; do
+      info "  rsync ${srv}"
       dest="${srv}:/var/www/${site}"
       log="${logprefix}.${site}.rsync.${srv}.log"
-      echo -e "\n# epoch ${EPOCHSECONDS}\n# $(date -R)" >>${log}
+      echo -e "\n# ${EPOCHSECONDS}\n# $(date -R)" >>${log}
       if ! rsync --verbose --recursive ~/tmp/hx/${site}/ ${dest} >>${log} 2>/dev/null; then
-        info "  NOT ok" >&2
-      else
-        info "  rsync ${site} log to ${srv}"
-        if ! rsync --verbose ${log} ${dest} &>/dev/null; then
-          info "  NOT ok" >&2
-        fi
+        info "    NOT ok" >&2
+      fi
+      info "  rsync ${srv} log"
+      if ! rsync ${log} ${dest} &>/dev/null; then
+        info "    NOT ok" >&2
       fi
     done
   fi
@@ -33,38 +35,38 @@ cd $(dirname $0)/..
 source ./hx/hx-lib.sh
 
 [[ -d ~/tmp/hx ]]
-logprefix=~/tmp/hx/$(basename $0)
 trap 'echo; echo stopping...; touch ~/tmp/hx/STOP-info' INT QUIT TERM EXIT
 
 info "pid $$"
 pit_stop info 0
+logprefix=~/tmp/hx/$(basename $0)
 
 while :; do
   #--------------------------------------------------------------------
   site="site01"
-  tags="coredump,issue,trace"
   srvs=""
+  tags="coredump,issue,trace"
 
-  info "${site} ${tags}"
+  info "${site}  tags:  ${tags}"
   if ! ./site-info.yaml --limit 'hx,!hix' --tags ${tags} -e '{ "infodir": "~/tmp/hx/'${site}'" }' \
     -e '{ "issue_since": "24 hours ago" }' -e '{ "trace_since": "24 hours ago" }' \
-    &>${logprefix}.${site}.ansible.${tags}.log; then
+    &>${logprefix}.${site}.ansible.log; then
     info "  NOT ok" >&2
   fi
-  sync_site $(eval echo ${srvs})
+  sync_site ${site} ${srvs}
   pit_stop info
 
   #--------------------------------------------------------------------
   site="site02"
-  tags="artefact"
   srvs=""
+  tags="artefact"
 
-  info "${site} ${tags}"
+  info "${site}  tags:  ${tags}"
   if ! ./site-info.yaml --limit 'hx,!hix' --tags ${tags} -e '{ "infodir": "~/tmp/hx/'${site}'" }' \
-    &>${logprefix}.${site}.ansible.${tags}.log; then
+    &>${logprefix}.${site}.ansible.log; then
     info "  NOT ok" >&2
   fi
-  sync_site $(eval echo ${srvs})
+  sync_site ${site} ${srvs}
   pit_stop info
 
   #--------------------------------------------------------------------
