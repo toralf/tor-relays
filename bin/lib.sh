e6b15db15a1e398e2b2a4b131d2aa30102b50cde
Toralf Förster <toralf.foerster@gmx.de>
2026-08-09 18:14:12 +0200

    delete: cleanup artefact too

--- bin/lib.sh
+++ bin/lib.sh
@@ -29,7 +29,7 @@ function cleanLocalDataEntries() {
 function cleanLocalDataFiles() {
   echo -e " deleting local data files ..."
   while read -r name; do
-    rm -f ~/tmp/tor-relays/{coredump,dmesg,kconfig,trace}/${name}{,.*}
+    rm -f ~/tmp/tor-relays/{artefact,coredump,dmesg,kconfig,trace}/${name}{,.*}
     rm -rf ~/tmp/tor-relays/fw/${name}/
     if [[ -z ${KEEP_TOR_KEYS-} ]]; then
       rm -rf ~/tmp/tor-relays/tor-identity/${name}/
