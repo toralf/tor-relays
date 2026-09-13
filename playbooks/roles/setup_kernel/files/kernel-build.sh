590ab192dca53f06f4a7452ea7e64c3eb8e3477f
Toralf Förster <toralf.foerster@gmx.de>
2026-09-13 10:15:59 +0200

    new variant using nftables

--- playbooks/roles/setup_kernel/files/kernel-build.sh
+++ playbooks/roles/setup_kernel/files/kernel-build.sh
@@ -44,7 +44,7 @@ ln -snf ${PWD} /usr/src/linux
 echo -e "\n$(date) reboot ...\n"
 if [[ ${1-} == "reboot" ]]; then
   i=12 # 2 min
-  # pgrep works for Debian >= 13 or Ubuntu >= 26.04
+  # "sshd-session" works for Debian >= 13 or Ubuntu >= 26.04
   while ((i--)) && pgrep -x sshd-session >/dev/null; do
     echo "finish ssh connections graceful ..."
     sleep 10
